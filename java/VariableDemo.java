1874b25541b588b313afc0af26d01f76ff0734df
Eugene Royce <eugeneroycesatorre@gmail.com>
2023-02-03 15:00:14 +0800

    Add files via upload

---
+++ java/VariableDemo.java
@@ -0,0 +1,14 @@
+public class VariableDemo{
+    public static void main(String[] args){
+        int meter = 2;
+        int centimeter;
+        String a = "hello";
+        String b = "world!";
+
+        centimeter = meter * 100;
+        System.out.println(meter + "m = " + centimeter + "cm");
+        System.out.print(a);
+        System.out.print(b);
+
+    }
+}
