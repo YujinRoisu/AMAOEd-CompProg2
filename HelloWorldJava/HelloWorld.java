6a70762069c188df1c8830f18e2fdcc62e818505
Eugene Royce <eugeneroycesatorre@gmail.com>
2023-02-03 14:59:10 +0800

    Add files via upload

---
+++ HelloWorldJava/HelloWorld.java
@@ -0,0 +1,35 @@
+//java comment 1sr hello program
+
+public class HelloWorld{//begin block 1
+byte b1 = -60; //byte data type is an 8-bit signed integer
+byte b2 = 101;
+short s1 = -129; //short data type is a 16-bit signed integer
+short s2 = 128;
+int i1 = -32769; //int data type is a 32-bit signed integer
+int i2 = 32768;
+long l1 = -32769L; //long data type is a 64-bit signed integer
+long l2 = 32768L;
+float f1 = -1.2345F; //float data type is a 32-bit IEEE 754 floating point
+float f2 = 3.4028235E+38F;
+double d1 = 1.23456; //double data type is a 64-bit IEEE 754 floating point
+double d2 = -12.3456D;
+double d3 = 12.3456D;
+boolean odd = true; //boolean data type represents 1 bit of information
+
+
+    public static void main(String args[]){//begin block 2
+	    System.out.println("Hello World!");
+        System.out.println("Welcome to Java! \nhehe");
+
+        System.out.println(42); //Displays 42
+        System.out.println(0b101010); //Displays 42
+        System.out.println(0x2A); //Displays 42
+        System.out.println(052); //Displays 42
+
+        System.out.println(21474836470L); // Displays 21474836470
+        System.out.println(0b101010L); // Displays 42
+        System.out.println(0237777777766L); //Displays 21474836470
+        System.out.println(0x4FFFFFFF6L); // Displays 21474836470        
+        
+	}// end block 1
+}// end block 2
